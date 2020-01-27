9b1b35fa0b1304f0b46c53645e5307e65024b3eb
Giovani <giovani.rdgs@gmail.com>
2020-01-27 13:52:48 -0300

    Verify JWT for access the routes

---
+++ src/config/verifyToken.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.json({error: 'Token not provided'})
+    }
+    const parts = authHeader.split(' ');
+    if (parts.length !== 2) {
+        return res.status(400).json({error: 'token malformated'});
+    }
+
+    const [ scheme, token ] = parts;
+
+    if (!/^Bearer$/.test(scheme)) {
+        return res.status(400).json({error: 'token malformated'});
+    }
+    jwt.verify(token, process.env.SECRET, (err, decode) => {
+        if (err) {
+            return res.json({error: err})
+        }
+        req.userId = decode.id;
+        req.typeUser = decode.typeUser
+        return next();
+    })
+}
