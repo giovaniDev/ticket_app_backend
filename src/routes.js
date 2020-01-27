a54544f1530fc2cbd94757c2f4757643f2c9c24a
Giovani <giovani.rdgs@gmail.com>
2020-01-27 13:54:06 -0300

    Routes

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const multer = require('multer');
+
+const RegisterUserController = require('./controller/RegisterUserController');
+const RegisterCompanyController = require('./controller/RegisterCompanyController');
+const AuthUserController = require('./controller/AuthUserController');
+const AuthCompanyController = require('./controller/AuthCompanyController');
+const UserController = require('./controller/UserController');
+const CompanyController = require('./controller/CompanyController');
+const PlanController = require('./controller/PlanController');
+
+const route = express.Router();
+
+const { storage, fileFilter } = require('./config/uploadCompanyConfig');
+const uploadCompany = multer({storage, fileFilter})
+
+const uploadUserConfig = require('./config/uploadUserConfig');
+const uploadUser = multer(uploadUserConfig);
+
+route.post('/register/company', uploadCompany.single('thumbnail') , RegisterCompanyController.store);
+route.post('/auth', AuthUserController.store);
+route.post('/authcompany', AuthCompanyController.store);
+
+const verifyToken = require('./config/verifyToken');
+route.use(verifyToken);
+
+route.get('/users', UserController.show);
+route.get('/companies', CompanyController.index)
+route.post('/companies/plans', PlanController.store)
+route.get('/companies/plans/:company_id', PlanController.index)
+
+module.exports = route;
