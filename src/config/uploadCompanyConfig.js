e9048e6966de5a634f26dbe36b2d4aaa286fa717
Giovani <giovani.rdgs@gmail.com>
2020-01-27 13:53:21 -0300

    config company for upload image

---
+++ src/config/uploadCompanyConfig.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+const path = require('path');
+
+module.exports = {
+    storage: multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, path.resolve(__dirname, '..', '..', 'uploads', 'company'))
+        },
+        filename: function (req, file, cb) {
+            console.log(req)
+            const ext = path.extname(file.originalname);
+                const name = path.basename(file.originalname, ext)
+                cb(null, `${name}-${Date.now()}${ext}`)
+        }
+    }),
+    fileFilter: (req, file, cb) => {
+        if (req.body.document_id.length !== 14) {
+            return cb(null, false);
+        } else {
+            cb(null, true)
+        }
+    }
+}
