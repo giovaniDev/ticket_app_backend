bb69d694419c74f5c9d2ac5ed0ba07985d55f548
Giovani <giovani.rdgs@gmail.com>
2020-01-29 14:57:07 -0300

    Adding static route for image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose  = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 mongoose.connect(process.env.DATABASE, {  useNewUrlParser: true, useUnifiedTopology: true } );
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'company')))
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'user')))
 app.use(require('./routes'))
 
 app.listen(3000);
