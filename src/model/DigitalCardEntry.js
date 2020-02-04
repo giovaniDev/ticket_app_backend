bbbf2b27feeeab558ca590a0a11b1b6b1510de0f
Giovani <giovani.rdgs@gmail.com>
2020-02-04 19:32:53 -0300

    new model, DIgitalCardEntry

---
+++ src/model/DigitalCardEntry.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const DigitalCardEntry = new mongoose.Schema({
+    company_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Company',
+        required: true
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    typePlan: {
+        type: String,
+        required: true
+    },
+    expireIn: {
+        type: String,
+        required: true
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Card', DigitalCardEntry);
+
